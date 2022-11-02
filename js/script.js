ac1aa82edcfe358eb4b5284867b37b4ee7819490
gab-che <gabriellachessa1@gmail.com>
2022-11-02 16:34:14 +0100

    added squares via js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
+const squareGrid = document.querySelector(".square_grid");
+
 for (let x = 1; x <= 100; x++){
     if(x % 3 === 0 && x % 5 === 0){
-        console.log("multiplo di tre e di cinque")
-    } else if(x % 3 === 0){
-        console.log("multiplo di tre");
+        squareGrid.innerHTML += '<div class="square square_fizz_buzz">FizzBuzz</div>';
+        console.log("fizzbuzz");
+    } else if (x % 3 === 0){
+        squareGrid.innerHTML += '<div class="square square_fizz">Fizz</div>';
+        console.log("fizz");
     } else if (x % 5 === 0){
-        console.log("multiplo di cinque")
+        squareGrid.innerHTML += '<div class="square square_buzz">Buzz</div>';
+        console.log("buzz");
     } else{
+        squareGrid.innerHTML += `<div class="square">${x}</div>`;
         console.log(x);
-    }
+    }   
 }
